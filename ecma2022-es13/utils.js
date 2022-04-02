868151e57260dd4f8c4e3da1dad54168e5f0796d
lhj <851681631@qq.com>
2022-04-02 23:42:47 +0800

    feat: ecma2022新特性学习,打卡完成

---
+++ ecma2022-es13/utils.js
@@ -0,0 +1,11 @@
+async function getList() {
+  return ["滨江区", "西湖区", "江干区", "上城区", "拱墅区", "余杭区"];
+}
+
+async function getMessage() {
+  return ["人生苦短, I want to make .py !!!"];
+}
+
+const MENU_LIST = ["首页", "关于我"];
+
+export { getList, getMessage, MENU_LIST };
